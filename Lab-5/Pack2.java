0e128bf609d117a9c9b12136e98e4fd6054daf17
Rathin Kumar Gorai <rthnkg@gmail.com>
2022-03-04 09:27:37 +0530

    04/03/2020

--- Lab-5/Pack2.java
+++ Lab-5/Pack2.java
@@ -1,5 +1,5 @@
-import Pack.*;
-public class Dog{
+import Animals.*;
+public class Pack2{
     public static void main(String args[]){
         Animal A = new Animal();
         A.run();
--- Lab-5/pack.java
+++ Lab-5/pack.java
@@ -1,9 +1,9 @@
 package Animals;
-class Pack{
+public class Pack{
     public void run(){
         System.out.println("Running");
     }
     public void eat(){
-        System.out.println("Eating");;
+        System.out.println("Eating");
     }
 }
